b86de3607108b6718662a9a0ad8f8250aef0cc06
Алексей Елютин <aleksei_elyutin@mail.ru>
2017-12-12 23:59:14 +0300

    release 1.0

--- Program1.pas
+++ Program1.pas
@@ -1,14 +1,33 @@
 ﻿program a1;
-//uses crt;
+uses crt;
+{$define pDEBUG} 
+//{$undef pDEBUG}
 
 Type 
-     PStudent = ^TStudent;
-     TStudent = record
+     PStudent = ^TStudent;    //Тип: Указатель на структуру TStudent
+     TStudent = record       //Тип: Структура TStudent
         id: integer;
         name: string;
         next: PStudent;
         end;
-      TBase = file of TStudent;
+        
+     FStudent = record       //Тип: Структура TStudent
+        id: integer;
+        name: string[10];
+        end;
+        
+     TBase = file of FStudent; //Тип: Файл структур TStudent
+
+{$ifdef pDEBUG}
+procedure pDebug(s:string);
+begin
+    writeln('**DEBUG: ',s, '**');
+end;
+{$else}
+procedure pDebug(s:string);
+begin
+end;
+{$endif}
 
 function createItem (_id: integer; _name: string ): PStudent;
 var temp_pointer:PStudent;
@@ -17,6 +36,7 @@ begin
     temp_pointer^.id := _id;
     temp_pointer^.name := _name;
     temp_pointer^.next := nil;
+    createItem := temp_pointer;
 end;
 
 // Процедура для осовобождения памяти по указателю на структуру 
@@ -25,26 +45,33 @@ end;
 procedure deleteItem (var _root:PStudent; item:Pstudent);
 var current, previous:Pstudent;
 begin
+    if item = nil then exit;
     if item = _root then
-    begin
-        _root := _root^.next;
+    begin       
         writeln ('Удаляется запись ', _root^.id,  _root^.name);
+        _root := _root^.next;
         dispose(item);
+        pDebug ('Корневая запись изменена');
         exit;
     end;
+    
+    previous := _root;
     current := _root^.next;
-    while not (current^.next=nil) do
+       
+     repeat
     begin
         if current = item then
-        begin
+        begin 
             previous^.next := current^.next;
-            writeln ('Удаляется запись ', current^.id,  current^.name);
-            dispose(current);
+            //writeln ('Удаляется запись ', current^.id,  current^.name);
+            dispose(item);
             exit;
         end;
         previous := current;
         current := current^.next;
-    end;
+    end
+    until (current=nil);
+    
 end;
 
 // Функция для поиска структуры по номеру.
@@ -53,17 +80,19 @@ end;
 function findById (_root:PStudent; _id:integer): PStudent;
 var current:Pstudent;
 begin
-    current := _root;
-    while not (current^.next=nil) do
+    current := _root;   
+    repeat
     begin
         if current^.id = _id then 
-        begin
-            findById := current;
-            exit;
-        end;
+                begin
+                    findById := current;
+                    exit;
+                end;
         current := current^.next;
-    end;
-    writeln ('Поиск по номеру: записей не найдено!');
+    end
+    until (current=nil);
+    
+    pDebug ('Поиск по номеру: записей не найдено!');
     findById := nil;
 end;
 
@@ -73,7 +102,7 @@ function findByName (_root:PStudent; _name:string): PStudent;
 var current:Pstudent;
 begin
     current := _root;
-    while not (current^.next=nil) do
+    repeat
     begin
         if current^.name = _name then 
         begin
@@ -81,7 +110,8 @@ begin
             exit;
         end;           
         current := current^.next;
-    end;
+    end
+    until (current=nil);
     writeln ('Поиск по имени: записей не найдено!');
     findByName := nil;
 end;
@@ -132,59 +162,253 @@ end;
 // с обновлением указателей
 // Принимает: указатель на корневую запись (изменяемый), указатель 
 // на добавляемую структуру
-procedure linkItem(var _root:PStudent; candidate: PStudent);
+procedure linkItem(var _root:PStudent; var candidate: PStudent);
 var current, previous, temproary:Pstudent;
 begin
-    previous := nil;
+    //previous := nil;
     if _root=nil then 
     begin
         _root:=candidate;
         exit;
     end;
     
-    if _root^.next = nil then
-    begin
-        if selectUpper(_root,candidate)= candidate then
-        temproary := _root;
-        _root := candidate;
-        _root^.next := temproary;
-        exit;
-    end;
+   
+        if (selectUpper(_root,candidate)=candidate) then    
+        begin
+            temproary := _root;
+            _root := candidate;
+            _root^.next := temproary;
+            exit;
+         end
+         else
+         begin
+            if _root^.next=nil then
+            begin
+                 _root^.next := candidate;
+                 exit;
+            end;
+         end;         
+
+        
+    previous := _root;
+    current := _root^.next;
     
-    current := _root;
-    while not (current^.next=nil) do
+    repeat
     begin
         if selectUpper(current,candidate)=candidate then
-        begin
-            if current=_root then
-            begin
-                temproary := _root;
-                _root := candidate;
-                _root^.next := temproary;
-                exit;
-            end;
+        begin 
             previous^.next := candidate;
-            candidate^.next := current;      
+            candidate^.next := current;
             exit;
         end;
-        candidate^.next := current;
         previous := current;
         current := current^.next;
-    end;
-    
-        
+    end
+    until (current=nil);
+    previous^.next := candidate;
+      
 end;
 
-procedure loadFromFile ()
-        
-var 
-    filename :string; s1, s2:string; f:TBase;
+procedure loadFromFile (var _root:PStudent; filename :string);       
+var tmp_pointer:PStudent; fileItem:FStudent;  f:TBase;
+begin 
+    if not FileExists(filename) then 
+    begin 
+        pDebug ('Файл пуст');
+        exit;
+    end
+    else 
+    begin 
+        assign(f,filename);   
+    end;
+    reset(f);
+    while not EOF(f) do
+    begin
+        read(f,fileItem);
+        tmp_pointer := createItem (fileItem.id, fileItem.name);
+        linkItem(_root, tmp_pointer);
+    end;
+    close(f);      
+end;
 
-begin //Тело программы
+procedure saveToFile (var _root:PStudent; filename :string);       
+var current:Pstudent; fileItem:FStudent;  f:TBase;
+begin 
+    assign(f,filename); 
+    rewrite(f); 
+    current := _root;
+    if not(_root=nil) then
+    begin
+        current := _root;
+    end
+    else
+    begin
+        pDebug('Список пуст!');
+        exit;
+    end;
+    repeat
+    begin
+        fileItem.id := current^.id;
+        fileItem.name := current^.name;
+        write(f,fileItem);
+        current := current^.next;
+    end
+    until (current=nil);
+    close(f);      
+end;
+
+procedure showList (_root:PStudent);
+var current:Pstudent;
+begin
+    clrscr();  
+    writeln ('Список в опреативной памяти:');
+    writeln (' _______________________________');
+    writeln ('|   Номер  |   Фамилия И.О.     |');
+    if not(_root=nil) then
+    begin
+        current := _root;
+    end
+    else 
+    begin
+        writeln ('Список пуст...');
+        writeln ('|_______________________________|');
+        readln();
+        exit;
+    end; 
+    repeat
+    begin
+        writeln ('| ',current^.id, '   ',current^.name);                   
+        current := current^.next;
+    end
+    until (current=nil);
+        writeln ('|_______________________________|');
+    readln();
+end;
+
+procedure drawMenu();
+begin
+    clrscr;
+    writeln('                      Главное меню                            ');
+    writeln('Выберите действие путем ввода соответствующей цифры в консоль:');
+    writeln('1.  Добавить запись...');
+    writeln('2.  Найти и удалить запись...');
+    writeln('3.  Вывести список на экран');
+    writeln('0.  Завершить работу');   
+end;
+
+procedure addItemDialog(var _root:PStudent);
+var tmp_pointer:PStudent; _name:string; var _id:integer;
+begin
+    clrscr;
+    writeln('                      Добавление записи                      ');
+    write(' Введите ФИО студента: ');
+    readln(_name);
+    write(' Введите номер зачетной книжки: ');
+    readln(_id);  
+    tmp_pointer := createItem (_id, _name);  
+    linkItem(_root, tmp_pointer);
+    write(' Создана запись: ');
+    writeln ('ФИО: ', tmp_pointer^.name, ', Номер зачетной книжки: ', tmp_pointer^.id);
+    if tmp_pointer = _root then write(' Добавлена в начало списка. '); 
+    if tmp_pointer^.next = nil then write(' Добавлена в конец списка. ')
+    else write(' Добавлена перед записью ', tmp_pointer^.next^.id, '  ', tmp_pointer^.next^.name);
+    readln();  
+end;
+
+procedure findAndDeleteDialog(var _root:PStudent);
+var tmp_pointer:PStudent; _name:string; _id:integer; action: integer;
+begin 
   
-filename := 'students.base';
-f
+    while (true) do
+	begin
+    	clrscr;
+        writeln('                      Поиск и удаление записи                      ');
+        writeln('Выберите действие путем ввода соответствующей цифры в консоль:');
+        writeln('1 - поиск по ФИО студента ');
+        writeln('2 - поиск по номеру зачетной книжки студента ');
+        writeln('3 - поиск по ФИО и номеру зачетной книжки студента ');
+        writeln('0 - выход в главное меню ');
+	   readln (action);
+       if action=0 then exit;
+	   case action of
+      1: begin
+            write('ФИО студента: '); 
+            readln (_name);
+            tmp_pointer := findByName(_root, _name);
+            if tmp_pointer=nil then
+            begin
+                writeln ('Запись c ФИО ', _name, ' не найдена! Повторите ввод.');
+                readln();
+            end
+            else break;
+         end;
+      2: begin
+            write(' Введите номер зачетной книжки: ');
+            readln(_id);
+            tmp_pointer := findById(_root, _id);
+            if tmp_pointer=nil then
+            begin
+                writeln ('Запись по номеру' ,_id, ' не найдена! Повторите ввод.');
+                readln();
+            end
+            else break;
+         end;
+      3: begin
+            write(' Введите номер зачетной книжки: ');
+            readln(_id);
+            write('ФИО студента: '); 
+            readln (_name);
+            tmp_pointer := findByNameAndId(_root, _name,_id);
+            if tmp_pointer=nil then
+            begin
+                writeln ('Запись c номером' ,_id, 'и именем ', _name, ' не найдена! Повторите ввод.');
+                readln();
+            end
+            else break;
+         end
+      end;    
+	end;
+          writeln ('Найдена запись:');
+      writeln ('ФИО: ', tmp_pointer^.name, ', Номер зачетной книжки: ', tmp_pointer^.id);
+      writeln ('Удалить запись? 1-удалить, 0-выход');
+      while (true) do
+      begin
+            readln (action);
+           if action=0 then exit;
+           if action=1 then
+           begin
+                deleteItem(_root, tmp_pointer);
+                break;
+           end;
+      end;  
+      writeln ('Запись удалена.'); read();
+ 
+end;
 
-      
+
+
+
+var root:PStudent; filename:string; tmp_pointer:PStudent; action: integer;  // Глобальные переменные
+begin //Тело программы
+    pDebug('DEBUGGING ENABLED');  
+    filename := 'students.base';
+    loadFromFile(root,filename);
+
+    while (true) do
+      begin
+        drawMenu();
+        readln (action);
+       if action=0 then break;
+       case action of
+           1: addItemDialog(root);
+           2: findAndDeleteDialog(root);
+           3: showList(root);
+       end;    
+	end;
+  
+    
+    saveToFile(root,filename);
+
+    
 
 end.
